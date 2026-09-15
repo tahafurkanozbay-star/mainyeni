0b5ca0a95fe3c368ed9a64e76089e57a13d9ec75
tahafurkanozbay-star <tahafurkan.ozbay@ogr.iu.edu.tr>
2026-09-15 15:59:16 +0300

    test(gis): cover event icon identity

--- Webclient.app/src/gis-engine/iconRegistry.test.js
+++ Webclient.app/src/gis-engine/iconRegistry.test.js
@@ -16,6 +16,7 @@ describe('shared GIS icon registry', () => {
     ['YeniSosyalHizmetlerQueryUrl', 'sosyal'],
     ['YeniTeknolojiMerkezleriQueryUrl', 'teknoloji'],
     ['AssemblyAreaQueryUrl', 'aciltoplanma'],
+    ['EventQueryUrl', 'etkinlik'],
     ['PharmacyQueryUrl', 'eczane'],
     ['TaxiQueryUrl', 'taksi']
   ])('uses the JSON registry for service key %s', (serviceKey, expectedId) => {
@@ -25,6 +26,7 @@ describe('shared GIS icon registry', () => {
   test.each([
     ['YeniKadinDanismaQueryUrl', 'kadin'],
     ['AssemblyAreaQueryUrl', 'aciltoplanma'],
+    ['EventQueryUrl', 'etkinlik'],
     ['PharmacyQueryUrl', 'eczane'],
     ['TaxiQueryUrl', 'taksi']
   ])('keeps list, 2D and 3D icon identity aligned for %s', (type, expectedKey) => {
