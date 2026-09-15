6e383dcf4dedb4538d0f90ec946d15ebb004c297
tahafurkanozbay-star <tahafurkan.ozbay@ogr.iu.edu.tr>
2026-09-15 15:31:16 +0300

    refactor(ui): harden numbering search flow

--- Webclient.app/src/Components/Query/NumberingQuery/NumberingQueryWindow.js
+++ Webclient.app/src/Components/Query/NumberingQuery/NumberingQueryWindow.js
@@ -1,268 +1,160 @@
 import React, { useEffect, useImperativeHandle, useState } from "react";
 import { Form } from "react-bootstrap";
-import { RiCloseCircleFill } from "react-icons/ri";
 import { LoggingBusiness } from "../../../Business/LoggingBusiness";
 import { NumberingQueryBusiness } from "../../../Business/NumberingQueryBusiness";
 import { Constants_ServiceResultType } from "../../../Core/Constants";
 import MapManager from "../../../Store/Managers/MapManager";
-import { DebugHelper } from "../../../Toolbox/DebugHelper";
 import { GisGraphicsHelper } from "../../../Toolbox/GisGraphicsHelper";
 import "./NumberingQueryWindow.css";
 import { CommonQueryWindowTools } from "../../Query/_Common/CommonQueryWindowTools";
 
+const INITIAL_QUERY = Object.freeze({ district: "", districtName: "", nbhood: "", nbhoodName: "", street: "", streetName: "", door: "" });
+
 export const NumberingQueryWindow = React.forwardRef((props, ref) => {
+    const [mapView, setMapView] = useState(null);
+    const [districtList, setDistrictList] = useState([]);
+    const [nbhoodList, setNbhoodList] = useState([]);
+    const [streetList, setStreetList] = useState([]);
+    const [doorList, setDoorList] = useState([]);
+    const [highlightGraphic, setHighlightGraphic] = useState(null);
+    const [query, setQuery] = useState({ ...INITIAL_QUERY });
+
+    const clearHighlight = () => {
+        if (highlightGraphic && mapView) {
+            GisGraphicsHelper.RemoveGraphics(mapView, highlightGraphic);
+            setHighlightGraphic(null);
+        }
+    };
 
     useImperativeHandle(ref, () => ({
-        id: props.id,visible:false, minimized:false,
-        OnShow:()=>{
-            props.windowManager.ShowWindow("sidebar")
-            
-        },
+        id: props.id,
+        visible: false,
+        minimized: false,
+        OnShow: () => props.windowManager.ShowWindow("sidebar"),
         OnClose: () => {
-            if(highlightGraphic!=null){
-                GisGraphicsHelper.RemoveGraphics(mapView, highlightGraphic);
-                setHighlightGraphic(null);
-            }
-    
+            clearHighlight();
+            setQuery({ ...INITIAL_QUERY });
+            setNbhoodList([]);
+            setStreetList([]);
+            setDoorList([]);
         }
-    }));
+    }), [props.id, props.windowManager, highlightGraphic, mapView]);
 
-    const [mapView, setMapView] = useState(null);
-
-    const [districtList, setDistrictList] = useState(null);
     useEffect(() => {
-
         props.windowManager.RegisterWindow(ref);
-        
-        const _mapView = MapManager.GetMapView();
-        setMapView(_mapView);
-
-        //Window Manager register window
-        ;
-
-        NumberingQueryBusiness.GetDistricts().then(_result => {
-
-            if (_result.type == Constants_ServiceResultType.Success) {
-                setDistrictList(_result.data);
-            }
+        setMapView(MapManager.GetMapView());
+        let active = true;
+        NumberingQueryBusiness.GetDistricts().then(result => {
+            if (active && result?.type === Constants_ServiceResultType.Success) setDistrictList(result.data || []);
+        }).catch(() => {
+            if (active) setDistrictList([]);
         });
+        return () => {
+            active = false;
+        };
+    }, [props.windowManager, ref]);
 
-    }, []);
+    const setQueryField = (field, value) => setQuery(current => ({ ...current, [field]: value }));
 
-    const [query, setQuery] = useState({ district: null, districtName: null, nbhood: null, nbhoodName: null, street: null, streetName:null, door: null });
-    const setQueryField = (_field, _value) => {
-        setQuery( query => {
-            return { ...query,[_field]: _value}
-         })
-    }
+    const zoomToObject = async (geometries, zoomLevel) => {
+        if (!mapView || !geometries?.length) return;
+        clearHighlight();
+        const projected = await Promise.all(geometries.map(geometry => GisGraphicsHelper.ProjectGeometry(geometry, "4326")));
+        await GisGraphicsHelper.ZoomToGeometry(mapView, projected, zoomLevel);
+        const graphics = await Promise.all(projected.map(geometry => GisGraphicsHelper.CreateGraphicFromGeometry(geometry)));
+        setHighlightGraphic(graphics);
+        GisGraphicsHelper.AddGraphics(mapView, graphics);
+    };
 
-    const [nbhoodList, setNbhoodList] = useState(null);
-    const cmbDistrict_OnChange = (e) => {
+    const onDistrictChange = async event => {
+        const districtId = event.target.value;
+        const districtName = event.target.selectedOptions[0]?.text || "";
+        setQuery(current => ({ ...current, district: districtId, districtName, nbhood: "", nbhoodName: "", street: "", streetName: "", door: "" }));
+        setNbhoodList([]);
+        setStreetList([]);
+        setDoorList([]);
+        if (!districtId) return;
 
-        const districtId = e.target.value;
-        const districtName= e.target.selectedOptions[0].text;
-
-        setQueryField("district", districtId);
-        setQueryField("districtName", districtName);
-
-
-        NumberingQueryBusiness.GetDistrictById(districtId).then((_districtResult)=>{
-            
-            if(_districtResult.type==Constants_ServiceResultType.Success){
-            
-                const _district=_districtResult.data[0];
-                
-                zoomToObject([_district.geometry], null)
-            }
-        });
-
-        setNbhoodList(null);
-        setStreetList(null);
-        setDoorList(null);
-
-        NumberingQueryBusiness.GetNeighborhoodsOfDistrict(districtId).then((_result) => {
-            if (_result.type == Constants_ServiceResultType.Success) {
-                setNbhoodList(_result.data);
-            }
-        });
-    }
-
-
-    const [highlightGraphic, setHighlightGraphic]=useState(null);
-    const zoomToObject=(_geometries, _zoomLevel)=>{
-
-        if(highlightGraphic!=null){
-            GisGraphicsHelper.RemoveGraphics(mapView, highlightGraphic);
+        const [districtResult, neighborhoodsResult] = await Promise.all([
+            NumberingQueryBusiness.GetDistrictById(districtId),
+            NumberingQueryBusiness.GetNeighborhoodsOfDistrict(districtId)
+        ]);
+        if (districtResult?.type === Constants_ServiceResultType.Success && districtResult.data?.[0]?.geometry) {
+            zoomToObject([districtResult.data[0].geometry], null);
         }
+        if (neighborhoodsResult?.type === Constants_ServiceResultType.Success) setNbhoodList(neighborhoodsResult.data || []);
+    };
 
-        let _projectPromises=[];
-        _geometries.forEach(_geometry => {
-            _projectPromises.push(GisGraphicsHelper.ProjectGeometry(_geometry, "4326"));
-        });
-        
-        let _graphicPromises=[];
-        Promise.all(_projectPromises).then((_projectedGeometries)=>{
+    const onNeighborhoodChange = async event => {
+        const nbhoodId = event.target.value;
+        const nbhoodName = event.target.selectedOptions[0]?.text || "";
+        setQuery(current => ({ ...current, nbhood: nbhoodId, nbhoodName, street: "", streetName: "", door: "" }));
+        setStreetList([]);
+        setDoorList([]);
+        if (!nbhoodId) return;
 
+        const [neighborhoodResult, streetsResult] = await Promise.all([
+            NumberingQueryBusiness.GetNeighborhoodById(nbhoodId),
+            NumberingQueryBusiness.GetStreets(nbhoodId)
+        ]);
+        if (neighborhoodResult?.type === Constants_ServiceResultType.Success && neighborhoodResult.data?.[0]?.geometry) {
+            zoomToObject([neighborhoodResult.data[0].geometry], null);
+        }
+        setStreetList(streetsResult?.data || []);
+    };
 
-            GisGraphicsHelper.ZoomToGeometry(mapView, _projectedGeometries, _zoomLevel);
+    const onStreetChange = async event => {
+        const streetId = event.target.value;
+        const streetName = event.target.selectedOptions[0]?.text || "";
+        setQuery(current => ({ ...current, street: streetId, streetName, door: "" }));
+        setDoorList([]);
+        if (!streetId) return;
 
-            _projectedGeometries.forEach(_geometry=> {
-                _graphicPromises.push(GisGraphicsHelper.CreateGraphicFromGeometry(_geometry));
-            });
+        const [centerLines, doorsResult] = await Promise.all([
+            NumberingQueryBusiness.GetStreetCenterLines(streetId),
+            NumberingQueryBusiness.GetDoors(streetId)
+        ]);
+        const geometries = (centerLines || []).map(item => item.geometry).filter(Boolean);
+        if (geometries.length) zoomToObject(geometries, null);
+        setDoorList(doorsResult?.data || []);
+    };
 
-            Promise.all(_graphicPromises).then(_graphics=>{
-                setHighlightGraphic(_graphics);
-                GisGraphicsHelper.AddGraphics(mapView, _graphics);
-            });
-        });
-
-    
-    }
-
-
-    const [streetList, setStreetList] = useState(null);
-    const cmbNbhood_OnChange = (e) => {
-        
-        const nbhoodId = e.target.value;
-        const nbhoodName= e.target.selectedOptions[0].text;
-
-        NumberingQueryBusiness.GetNeighborhoodById(nbhoodId).then((_nbhoodResult)=>{
-            if(_nbhoodResult.type==Constants_ServiceResultType.Success){
-                const _nbhood=_nbhoodResult.data[0];
-                zoomToObject([_nbhood.geometry],null);
-            }
-        });
-
-
-        setQueryField("nbhood", nbhoodId);
-        setQueryField("nbhoodName", nbhoodName);
-
-        setStreetList(null);
-        setDoorList(null);
-
-        NumberingQueryBusiness.GetStreets(nbhoodId).then((_result) => {
-            setStreetList(_result.data);
-        });
-    }
-
-    const [doorList, setDoorList] = useState(null);
-    const cmbStreet_OnChange = (e) => {
-        
-        const streetId = e.target.value;
-        const streetName= e.target.selectedOptions[0].text;
-
-        setQueryField("street", streetId);
-        setQueryField("streetName", streetName);
-
-
-        NumberingQueryBusiness.GetStreetCenterLines(streetId).then((_centerLines)=>{
-            
-            const _geometries=[];
-            _centerLines.forEach(_centerLine => {
-                _geometries.push(_centerLine.geometry);
-            });
-            zoomToObject(_geometries);
-        })
-
-        setDoorList(null);
-
-        NumberingQueryBusiness.GetDoors(streetId).then((_result) => {
-            setDoorList(_result.data);
-        });
-    }
-
-    const [door, setDoor] = useState(null);
-    const cmbDoor_OnChange = (e) => {
-
-        const doorId = e.target.value;
-
+    const onDoorChange = async event => {
+        const doorId = event.target.value;
         setQueryField("door", doorId);
+        if (!doorId) return;
+        LoggingBusiness.CreateClientLog("Numarataj/Sorgu", `${query.districtName}/${query.nbhoodName}/${query.streetName}/${doorId}`);
+        const result = await NumberingQueryBusiness.GetDoorById(doorId);
+        if (result?.type === Constants_ServiceResultType.Success && result.data?.[0]?.geometry) zoomToObject([result.data[0].geometry], 18);
+    };
 
-        LoggingBusiness.CreateClientLog("Numarataj/Sorgu", query.districtName+"/"+query.nbhoodName+"/"+query.streetName+"/"+query.door );
-
-        NumberingQueryBusiness.GetDoorById(doorId).then((_result) => {
-
-            if (_result.type == Constants_ServiceResultType.Success) {
-                
-                const door=_result?.data[0];
-                setDoor(door);
-                zoomToObject([door.geometry], 18);
-
-            }
-        });
-    }
-
+    const select = (id, label, value, onChange, items, valueKey, labelKey) => (
+        <Form.Group>
+            <label className="form-label form-label-white" htmlFor={id}>{label}</label>
+            <select id={id} className="form-select" value={value} onChange={onChange}>
+                <option value="">Seçiniz..</option>
+                {items.map(item => <option key={item.attr?.[valueKey]} value={item.attr?.[valueKey]}>{item.attr?.[labelKey]}</option>)}
+            </select>
+        </Form.Group>
+    );
 
     return (
-    <div className="common-query-window common-query-window-right"
-        style={{ visibility: props.windowManager.IsVisible(props.id) ? 'visible' : 'hidden' }}>
-        <div className="common-query-window-header">
-            <img className="common-query-window-header-icon" src="images/icons/toolbar/adresarama.png"></img>
-            <span>Adres Arama</span>
-            <CommonQueryWindowTools
-                windowManager={props.windowManager}
-                windowId={props.id}
-                showNearbySearch={false}
-                showMapSelect={false}
-                setQueryField={(e) => { }}
-                query={null} />
-        </div>
-
-         <div className="common-query-window-body">
-                <Form>
-                    <Form.Group>
-                        <label className="form-label form-label-white">İlçe</label>
-                        <select className="form-select" onChange={((e) => cmbDistrict_OnChange(e))}>
-                            <option>Seçiniz..</option>
-                            {
-                                districtList?.map(_item => {
-                                    return <option value={_item.attr.id}>{_item.attr.ad}</option>
-                                })
-                            }
-                        </select>
-                    </Form.Group>
-                    <Form.Group>
-                        <label className="form-label form-label-white">Mahalle</label>
-                        <select className="form-select" onChange={((e) => cmbNbhood_OnChange(e))}>
-                            <option>Seçiniz..</option>
-                            {
-                                nbhoodList?.map(_item => {
-                                    return <option value={_item.attr.id}>{_item.attr.ad}</option>
-                                })
-                            }
-                        </select>
-                    </Form.Group>
-
-                    <Form.Group>
-                        <label className="form-label form-label-white">Cadde/Sokak</label>
-                        <select className="form-select" onChange={((e) => cmbStreet_OnChange(e))}>
-                            <option>Seçiniz..</option>
-                            {
-                                streetList?.map(_item => {
-                                    return <option value={_item.attr.yolid}>{_item.attr.ad}</option>
-                                })
-                            }
-                        </select>
-                    </Form.Group>
-
-                    <Form.Group>
-                        <label className="form-label form-label-white">Bina No</label>
-                        <select className="form-select" onChange={((e) => cmbDoor_OnChange(e))}>
-                            <option>Seçiniz..</option>
-                            {
-                                doorList?.map(_item => {
-                                    return <option value={_item.attr.id}>{_item.attr.kapino}</option>
-                                })
-                            }
-                        </select>
-                    </Form.Group>
-                </Form>
-
+        <div className="common-query-window common-query-window-right" style={{ visibility: props.windowManager.IsVisible(props.id) ? "visible" : "hidden" }}>
+            <div className="common-query-window-header">
+                <img className="common-query-window-header-icon" src="images/icons/toolbar/adresarama.png" alt="" aria-hidden="true" />
+                <span>Adres Arama</span>
+                <CommonQueryWindowTools windowManager={props.windowManager} windowId={props.id} showNearbySearch={false} showMapSelect={false} setQueryField={() => {}} query={null} />
             </div>
-
-    </div>);
-
-
-
-    
+            <div className="common-query-window-body">
+                <Form>
+                    {select(`${props.id}-district`, "İlçe", query.district, onDistrictChange, districtList, "id", "ad")}
+                    {select(`${props.id}-neighborhood`, "Mahalle", query.nbhood, onNeighborhoodChange, nbhoodList, "id", "ad")}
+                    {select(`${props.id}-street`, "Cadde/Sokak", query.street, onStreetChange, streetList, "yolid", "ad")}
+                    {select(`${props.id}-door`, "Bina No", query.door, onDoorChange, doorList, "id", "kapino")}
+                </Form>
+            </div>
+        </div>
+    );
 });
+
+NumberingQueryWindow.displayName = "NumberingQueryWindow";
