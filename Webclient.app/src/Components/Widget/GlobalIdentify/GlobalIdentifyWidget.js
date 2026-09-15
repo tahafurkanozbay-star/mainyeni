63c65d7f0af253dcc669c7a69f7c3701423445b7
tahafurkanozbay-star <tahafurkan.ozbay@ogr.iu.edu.tr>
2026-09-15 15:22:15 +0300

    refactor(ui): harden identify results lifecycle

--- Webclient.app/src/Components/Widget/GlobalIdentify/GlobalIdentifyWidget.js
+++ Webclient.app/src/Components/Widget/GlobalIdentify/GlobalIdentifyWidget.js
@@ -3,7 +3,7 @@ import { loadModules } from "esri-loader";
 import MapManager from "../../../Store/Managers/MapManager";
 import { CommonQueryWindowTools } from "../../Query/_Common/CommonQueryWindowTools";
 import "./GlobalIdentifyWidget.css";
-import { BiInfoCircle, BiZoomIn } from "react-icons/bi";
+import { BiZoomIn } from "react-icons/bi";
 import { IsNull } from "../../../Toolbox/ObjectHelper";
 import { ContainerLoading, NoResultsFound } from "../../Common/Loading";
 import { Accordion, Button, Tab, Tabs } from "react-bootstrap";
@@ -11,217 +11,103 @@ import { GisGraphicsHelper } from "../../../Toolbox/GisGraphicsHelper";
 import { LoggingBusiness } from "../../../Business/LoggingBusiness";
 
 export const GlobalIdentifyWidget = React.forwardRef((props, ref) => {
-
-    useImperativeHandle(ref, () => ({
-
-        id: props.id, visible: false, minimized: false,
-        OnShow: () => {
-
-            //startIdentify();
-            const event = MapManager.GetMapClickEvent();
-            executeIdentify(event);
-        },
-        OnClose: () => {
-
-            GisGraphicsHelper.RemoveAllGraphics(mapView);
-            cancelIdentify();
-            setResults();
-
-        }
-    }));
-
     const [mapView, setMapView] = useState(null);
-    useEffect(() => {
-        
-        props.windowManager.RegisterWindow(ref);
-
-        const _mapView = MapManager.GetMapView();
-        setMapView(_mapView);
-
-    }, []);
-
-
-    const [mapClickEvent, setMapClickEvent] = useState(null);
-    const startIdentify = () => {
-
-        //mapView.popup.autoOpenEnabled = false;
-
-        if (mapClickEvent == null) {
-
-            const _mapClickEvent = mapView.on("click", function (event) {
-                executeIdentify(event);
-            });
-
-            setMapClickEvent(_mapClickEvent);
-        }
-    }
-
-    const cancelIdentify = () => {
-        mapClickEvent?.remove();
-        setMapClickEvent(null);
-    }
-
-
     const [results, setResults] = useState(null);
     const [loading, setLoading] = useState(false);
 
-    const executeIdentify = (_event) => {
+    const executeIdentify = async event => {
+        const currentMapView = MapManager.GetMapView();
+        if (!event?.mapPoint || !currentMapView?.map) {
+            setResults([]);
+            return;
+        }
 
-        loadModules(["esri/rest/identify", "esri/rest/support/IdentifyParameters"]).then(([identify, IdentifyParameters]) => {
+        setLoading(true);
+        try {
+            const [identify, IdentifyParameters] = await loadModules(["esri/rest/identify", "esri/rest/support/IdentifyParameters"]);
+            const requests = currentMapView.map.allLayers.items
+                .filter(layer => layer.visible && !IsNull(layer.title) && layer.url)
+                .map(layer => identify.identify(layer.url, new IdentifyParameters({
+                    returnGeometry: true,
+                    geometry: event.mapPoint,
+                    tolerance: 3,
+                    mapExtent: currentMapView.extent
+                })));
 
-            let promises = [];
-            setLoading(true);
-
-
-            mapView?.map?.allLayers.items.forEach(_layer => {
-
-                if (_layer.visible) {
-
-                    if (!IsNull(_layer.title)) {
-
-                        let params = new IdentifyParameters({
-                            returnGeometry: true,
-                            geometry: _event.mapPoint,
-                            tolerance: 3,
-                            mapExtent: mapView.extent
-                        });
-
-                        let url = _layer.url;
-
-                        promises.push(identify.identify(url, params));
-
-                    }
-
-                }
-            });
-
-            Promise.allSettled(promises).then((responses) => {
-
-
-                let resultArray = [];
-                responses.forEach((response) => {
-
-                    if (response.status == 'fulfilled') {
-                        if (response?.value.results?.length > 0) {
-                            resultArray.push({
-                                layerId: response.value.results[0].layerId,
-                                layerName: response.value.results[0].layerName,
-                                features: response.value.results
-                            });
-
-                        }
-                    }
-
-
+            const responses = await Promise.allSettled(requests);
+            const nextResults = [];
+            responses.forEach(response => {
+                if (response.status !== 'fulfilled' || !response.value?.results?.length) return;
+                nextResults.push({
+                    layerId: response.value.results[0].layerId,
+                    layerName: response.value.results[0].layerName,
+                    features: response.value.results
                 });
-
-                setResults(resultArray);
-                setLoading(false);
-
             });
+            setResults(nextResults);
+        } catch (error) {
+            console.error("Identify request failed", error);
+            setResults([]);
+        } finally {
+            setLoading(false);
+        }
+    };
 
-        });
+    useImperativeHandle(ref, () => ({
+        id: props.id,
+        visible: false,
+        minimized: false,
+        OnShow: () => executeIdentify(MapManager.GetMapClickEvent()),
+        OnClose: () => {
+            GisGraphicsHelper.RemoveAllGraphics(MapManager.GetMapView());
+            setResults(null);
+        }
+    }), [props.id]);
 
-    }
+    useEffect(() => {
+        props.windowManager.RegisterWindow(ref);
+        setMapView(MapManager.GetMapView());
+    }, [props.windowManager, ref]);
 
+    const goToItem = async item => {
+        LoggingBusiness.CreateClientLog("Bilgi al/zoom", item);
+        const projectedGeometry = await GisGraphicsHelper.ProjectGeometry(item.feature.geometry, "4326");
+        const graphic = await GisGraphicsHelper.CreateGraphicFromGeometry(projectedGeometry);
+        GisGraphicsHelper.RemoveAllGraphics(mapView);
+        GisGraphicsHelper.AddGraphics(mapView, graphic);
+        GisGraphicsHelper.ZoomToGeometry(mapView, graphic, null);
+        if (window.screen.width < 768) props.windowManager.ToggleMinimiseWindow(props.id);
+    };
 
-    const goToItem = (_item) => {
-
-        LoggingBusiness.CreateClientLog("Bilgi al/zoom", _item);
-
-        GisGraphicsHelper.ProjectGeometry(_item.feature.geometry, "4326").then((_projectedGeometry) => {
-
-            GisGraphicsHelper.CreateGraphicFromGeometry(_projectedGeometry).then(_graphic => {
-
-                GisGraphicsHelper.RemoveAllGraphics(mapView);
-                GisGraphicsHelper.AddGraphics(mapView, _graphic);
-                GisGraphicsHelper.ZoomToGeometry(mapView, _graphic, null);
-
-                //LoggingBusiness.CreateClientLog("cityblockparcelquery-zoomtoparcel", item);
-
-                if (window.screen.width < 768) {
-                    props.windowManager.ToggleMinimiseWindow(props.id);
-                }
-
-            });
-        });
-    }
-
-
-    return (<div className="common-query-window common-query-window-right"
-        style={{ visibility: props.windowManager.IsVisible(props.id) ? 'visible' : 'hidden' }}>
-        <div className="common-query-window-header">
-            <img className="common-query-window-header-icon" src="images/icons/toolbar/bilgi.png"></img>
-            <span>Bilgi Al</span>
-            <CommonQueryWindowTools
-                windowManager={props.windowManager}
-                windowId={props.id}
-                showNearbySearch={false}
-                showMapSelect={false}
-                setQueryField={(e) => { }}
-                query={null} />
-        </div>
-        <div className="common-query-window-body layer-list-window-body">
-
-            {
-                /*
-                
-            <div className="global-identify-message-container">
-                <BiInfoCircle className="global-identify-message-icon" />
-                <span className="global-identify-message-text">Bilgi almak için haritaya tıklayın</span>
+    return (
+        <div className="common-query-window common-query-window-right" style={{ visibility: props.windowManager.IsVisible(props.id) ? 'visible' : 'hidden' }}>
+            <div className="common-query-window-header">
+                <img className="common-query-window-header-icon" src="images/icons/toolbar/bilgi.png" alt="" aria-hidden="true" />
+                <span>Bilgi Al</span>
+                <CommonQueryWindowTools windowManager={props.windowManager} windowId={props.id} showNearbySearch={false} showMapSelect={false} setQueryField={() => {}} query={null} />
             </div>
-            */
-            }
-
-            <div className="global-identify-results-container">
-                {
-                    loading ? <ContainerLoading /> :
-                        results?.length == 0 ? <NoResultsFound /> :
-                            <>
-                                <Accordion defaultActiveKey={-1}>
-                                    {
-                                        results?.map((_resultGroup, _resultGroupIndex) => {
-                                            return <Accordion.Item eventKey={_resultGroupIndex}
-                                                className="global-identify-results-accordion-item">
-                                                <Accordion.Header className="global-identify-results-accordion-item-header">
-                                                    <span>{_resultGroup.layerName} ({_resultGroup.features?.length})</span>
-                                                </Accordion.Header>
-                                                <Accordion.Body>
-                                                    <Tabs>
-                                                        {
-                                                            _resultGroup.features?.map((_feature, _featureIndex) => {
-                                                                return <Tab title={_feature.feature.attributes.OBJECTID}
-                                                                    eventKey={_featureIndex}>
-                                                                    <div className="global-identify-result-item-container">
-                                                                        <div className="global-identify-result-item-row">
-
-                                                                            <Button className="w-100 form-button" onClick={(e) => goToItem(_feature)}>
-                                                                                <BiZoomIn />&nbsp;&nbsp;
-                                                                                Haritada Göster</Button>
-                                                                        </div>
-                                                                        {
-                                                                            Object.entries(_feature.feature.attributes).map(_kvArray => {
-                                                                                return <div className="global-identify-result-item-row">
-                                                                                    <div className="global-identify-result-item-row-label"><strong>{_kvArray[0]}</strong></div>
-                                                                                    <div className="global-identify-result-item-row-text">{_kvArray[1]}</div>
-                                                                                </div>
-                                                                            })
-                                                                        }
-                                                                    </div>
-
-
-                                                                </Tab>
-                                                            })
-                                                        }
-                                                    </Tabs>
-                                                </Accordion.Body>
-                                            </Accordion.Item>
-                                        })
-                                    }
-                                </Accordion>
-                            </>
-                }
+            <div className="common-query-window-body layer-list-window-body">
+                <div className="global-identify-results-container">
+                    {loading ? <ContainerLoading /> : results?.length === 0 ? <NoResultsFound /> :
+                        <Accordion defaultActiveKey={-1}>
+                            {results?.map((resultGroup, groupIndex) => <Accordion.Item key={`${resultGroup.layerId}-${groupIndex}`} eventKey={groupIndex} className="global-identify-results-accordion-item">
+                                <Accordion.Header className="global-identify-results-accordion-item-header"><span>{resultGroup.layerName} ({resultGroup.features?.length})</span></Accordion.Header>
+                                <Accordion.Body>
+                                    <Tabs>
+                                        {resultGroup.features?.map((feature, featureIndex) => <Tab key={`${feature.feature.attributes.OBJECTID ?? featureIndex}`} title={feature.feature.attributes.OBJECTID ?? featureIndex + 1} eventKey={featureIndex}>
+                                            <div className="global-identify-result-item-container">
+                                                <div className="global-identify-result-item-row"><Button className="w-100 form-button" onClick={() => goToItem(feature)}><BiZoomIn aria-hidden="true" />&nbsp;&nbsp;Haritada Göster</Button></div>
+                                                {Object.entries(feature.feature.attributes).map(([key, value]) => <div className="global-identify-result-item-row" key={key}><div className="global-identify-result-item-row-label"><strong>{key}</strong></div><div className="global-identify-result-item-row-text">{String(value ?? '')}</div></div>)}
+                                            </div>
+                                        </Tab>)}
+                                    </Tabs>
+                                </Accordion.Body>
+                            </Accordion.Item>)}
+                        </Accordion>}
+                </div>
             </div>
         </div>
-    </div>);
+    );
 });
+
+GlobalIdentifyWidget.displayName = "GlobalIdentifyWidget";
