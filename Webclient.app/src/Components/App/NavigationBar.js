b4b81e6ed7892bd6ffea56c6ea7da8435338aca7
tahafurkanozbay-star <tahafurkan.ozbay@ogr.iu.edu.tr>
2026-09-15 12:56:51 +0300

    refactor(ui): simplify accessible enterprise header

--- Webclient.app/src/Components/App/NavigationBar.js
+++ Webclient.app/src/Components/App/NavigationBar.js
@@ -1,232 +1,68 @@
 import React, { useEffect, useState } from "react";
-import Store from "../../Store/Store";
-import { DatetimeHelper } from "../../Toolbox/DatetimeHelper";
 import { AppConfig } from "../../Core/AppConfig";
+import { Constants_ConfigKeys } from "../../Core/Constants";
 import { LocalStorageHelper } from "../../Toolbox/LocalStorageHelper";
-import { Constants_ConfigKeys, Constants_MessageType, Constants_UserMesssages } from "../../Core/Constants";
-import { BiAbacus, BiEnvelope, BiTargetLock } from "react-icons/bi";
-import { Button } from "react-bootstrap";
-import { MessageBar } from "../Common/MessageBar";
-import "./NavigationBar.css";
-import { CircleLoading } from "../Common/Loading";
-import { FulltextSearchQuery } from "../Query/FulltextSearchQuery/FulltextSearchQuery";
-import { LoggingBusiness } from "../../Business/LoggingBusiness";
-import MapManager from "../../Store/Managers/MapManager";
-import { GisGraphicsHelper } from "../../Toolbox/GisGraphicsHelper";
 import { CompanyLogo } from "./CompanyLogo";
+import "./NavigationBar.css";
 
 export function NavigationBar(props) {
-
-    const [themeIsLight, setThemeIsLight] = useState(false);
-    const [cssLink, setCssLink] = useState(null);
+    const [query, setQuery] = useState("");
 
     useEffect(() => {
-
-        const isLight = LocalStorageHelper.Get(Constants_ConfigKeys.THEME_CHOICE) ?? false;
-        switchTheme(isLight);
-
+        const storedTheme = LocalStorageHelper.Get(Constants_ConfigKeys.THEME_CHOICE);
+        if (storedTheme == null) return;
+        document.documentElement.dataset.experienceTheme = storedTheme ? "light" : "dark";
     }, []);
 
-    const switchTheme = (_isLight) => {
-
-        setThemeIsLight(_isLight);
-        LocalStorageHelper.Set(Constants_ConfigKeys.THEME_CHOICE, _isLight);
-
-        let head = document.head;
-
-        if (_isLight) {
-
-            let link = document.createElement("link");
-
-            link.type = "text/css";
-            link.rel = "stylesheet";
-            link.href = "styles.light.css";
-
-            head.appendChild(link);
-
-            setCssLink(link);
-
-        }
-        else {
-            if (cssLink) {
-                head.removeChild(cssLink);
-                setCssLink(null);
-            }
-        }
-    }
-
-
-    const printMap = () => {
-
-        let mapView = Store.getState().Map.View;
-
-        mapView.takeScreenshot({
-            width: 1920,
-            height: 1080
-        }).then(function (screenshot) {
-
-            let html = printHtml(screenshot.dataUrl);
-            let Pagelink = "about:blank";
-            let pwa = window.open(Pagelink, "_new");
-            pwa.document.open();
-            pwa.document.write(html);
-            pwa.document.close();
-
-        });
-
-    }
-
-    const printHtml = (imageSrc) => {
-
-
-        return "<html><head><script>function step1(){setTimeout('step2()', 10);}" +
-            "function step2(){window.print();window.close()}\n" +
-            "</script></head><body onload='step1()'>\n" +
-            "<div style='border:1px solid gray;'>" +
-            "<div style='font-size:24px;font-family:Arial,\"SegoeUI\";padding:5px;'>" + AppConfig.App.Title1 + " | " + AppConfig.App.Title2 + " (" + DatetimeHelper.GetFormatted(new Date()) + ")</div>" +
-            "<div style='border:1px solid gray;padding:0;margin:0;'>" +
-            "<img style='width:100%' src='" + imageSrc + "' />" +
-            "</div>" +
-            "</div>" +
-            +"</body></html>";
-    }
-
-
-    const refreshPage = (e) => {
-        window.location.reload();
-    }
-
-    const [activeblockstyle, setActiveblockstyle] = useState(null);
-    const setActiveWindow = (_windowid) => {
-
-        props.windowManager.ShowWindow(_windowid);
-
-        if (_windowid == "cityblockparcel-query-window") {
-            setActiveblockstyle({
-                display: 'block',
-                left: '210px'
-            });
-        }
-        else if (_windowid == "numbering-query-window") {
-            setActiveblockstyle({
-                display: 'block',
-                left: '75px'
-            });
-        }
-        else {
-            setActiveblockstyle(null);
-        }
-    }
-
-
-    const getUserLocation=()=>{
-        
-        const mapConfig = MapManager.GetMapConfiguration();
-        //let location = { x: mapConfig.Centerx, y: mapConfig.Centery }; //TODO: Geçici olarak gölbaşı merkeze ayarlandı değiştirilecek
-        let location = { x: 32.80409955978453, y: 39.94494728389463 };
-
-        if (!navigator.geolocation) {
-            props.windowManager.ShowMessage(Constants_MessageType.Error, Constants_UserMesssages.LOCATION_REJECTED);
-            createLocation(location);
-        }
-        else{
-            props.windowManager.ShowMessage(Constants_MessageType.Success, Constants_UserMesssages.LOCATION_ALLOWED);
-
-            navigator.geolocation.getCurrentPosition((_location) => {
-
-                location = {
-                    x: _location.coords.longitude,
-                    y: _location.coords.latitude,
-                };
-
-                createLocation(location);
-                
-
-            }, (error) => {
-
-                props.windowManager.ShowMessage(Constants_MessageType.Error, Constants_UserMesssages.LOCATION_REJECTED);
-                createLocation(location);
-            });
-        }
-    }
-
-    const createLocation = (_location) => {
-
-        GisGraphicsHelper.CreatePoint(_location).then((_point) => {
-
-            const mapView = MapManager.GetMapView();
-            GisGraphicsHelper.CreateGraphicFromGeometry(_point, null).then((_graphic) => {
-                MapManager.AddGraphics(_graphic, true);
-                GisGraphicsHelper.ZoomToGeometry(mapView, _point, 15);
-            });
-        });
-    }
-
-
-
-    const openReportWindow = (e) => {
-
-        LoggingBusiness.CreateClientLog("Rapor Sorgu", "");
-        const url = "https://cbs.ankara.bel.tr/portal/apps/dashboards/ec8cb7663d5942d49625a7660bd0570c";
-        window.open(url, "_blank");
-
-    }
-    const [isSidebarOpenGENEL, setIsSidebarOpenGENEL] = useState(true);
-    const [query, setQuery] = useState("");
-    const openGenelAramaWindow = () => {
-        setIsSidebarOpenGENEL(false); 
-         props.windowManager.ShowWindow("genelarama-query-window", query);
+    const openSearch = () => {
+        props.windowManager.ShowWindow("genelarama-query-window", { name: query.trim() });
     };
 
+    const onSearchKeyDown = event => {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            openSearch();
+        }
+        if (event.key === "Escape") {
+            setQuery("");
+            event.currentTarget.blur();
+        }
+        if ((event.ctrlKey || event.metaKey) && event.key.toLowerCase() === "k") {
+            event.preventDefault();
+            window.dispatchEvent(new CustomEvent("kentrehberi:command", { detail: { name: "command-palette" } }));
+        }
+    };
 
     return (
-        <>
-            <div className="mainbar-container">
-                <div className="row">
-
-                    <div className="col-12 .mainbar-apptitle">
-
-                        <div className="mainbar">
-
-
+        <header className="mainbar-container" role="banner" aria-label="Kent Rehberi üst gezinme">
+            <div className="row h-100">
+                <div className="col-12 h-100">
+                    <div className="mainbar">
                         <div className="mainbar-logo">
-                                <a href="https://www.ankara.bel.tr/" target="_blank" rel="noopener noreferrer">
-                               <img src="images/abblogo.svg" alt="Ankara Büyükşehir Belediyesi Logosu" title={"v" + AppConfig.App.Version} />
-                                </a>
-                                </div>
-                                <a href="https://kentrehberi.ankara.bel.tr" target="_blank" rel="noopener noreferrer">
-                                <span className="mainbar-text">KENT REHBERİ</span>
-                                </a>
-                                <div className="ns-input">
-                           <label className="ns-input-group" style={{ marginRight: "10px" }}>
-                             <input
-                                type="text"
-                                placeholder="Ara..."
-                                value={query.name}
-                                onChange={(e) => setQuery({ name: e.target.value })}
-                                onKeyPress={(e) => {
-                                       if (e.key === 'Enter') {
-                                          openGenelAramaWindow(); // Enter tuşuna basıldığında arama yap
-                                          }
-                               }}  />
-                            <img
-                              src="../images/search.svg"
-                              alt="search"
-                              onClick={openGenelAramaWindow}
-                               />
-                            </label>
-</div>
-                        
-                            <div className="mainbar-right">
-                                <CompanyLogo/> 
-                            </div>
+                            <a href="https://www.ankara.bel.tr/" target="_blank" rel="noopener noreferrer" aria-label="Ankara Büyükşehir Belediyesi ana sayfası">
+                                <img src="images/abblogo.svg" alt="Ankara Büyükşehir Belediyesi" title={`v${AppConfig.App.Version}`} />
+                            </a>
                         </div>
-
+                        <a href="https://kentrehberi.ankara.bel.tr" target="_blank" rel="noopener noreferrer" aria-label="Kent Rehberi ana sayfası">
+                            <span className="mainbar-text">KENT REHBERİ</span>
+                        </a>
+                        <form className="ns-input" role="search" onSubmit={event => { event.preventDefault(); openSearch(); }}>
+                            <label className="experience-sr-only" htmlFor="kentrehberi-global-search">Kent Rehberi genel arama</label>
+                            <div className="ns-input-group">
+                                <input id="kentrehberi-global-search" type="search" placeholder="Adres, yer veya katman ara…" value={query} onChange={event => setQuery(event.target.value)} onKeyDown={onSearchKeyDown} autoComplete="off" />
+                                <button type="submit" className="kr-search-submit" aria-label="Aramayı başlat" title="Ara"><span aria-hidden="true">⌕</span></button>
+                            </div>
+                            <span className="kr-search-hint" aria-hidden="true"><kbd>Ctrl</kbd><span>+</span><kbd>K</kbd></span>
+                        </form>
+                        <div className="mainbar-right" aria-label="Üst menü">
+                            <button type="button" className="kr-header-shortcut" onClick={() => window.dispatchEvent(new CustomEvent("kentrehberi:command", { detail: { name: "command-palette" } }))} aria-label="Komut merkezini aç" title="Komut merkezi (Ctrl K)">⌘K</button>
+                            <CompanyLogo />
+                        </div>
                     </div>
                 </div>
-
             </div>
-
-        </>
+        </header>
     );
 }
+
+export default NavigationBar;
