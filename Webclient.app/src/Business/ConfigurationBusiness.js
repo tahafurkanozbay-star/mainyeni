5c4f487b8ccea214267e552ab19e9c27fb5a7f2f
tahafurkanozbay-star <tahafurkan.ozbay@ogr.iu.edu.tr>
2026-09-15 15:30:59 +0300

    feat(platform): use resilient client for public bootstrap

--- Webclient.app/src/Business/ConfigurationBusiness.js
+++ Webclient.app/src/Business/ConfigurationBusiness.js
@@ -1,53 +1,35 @@
-import axios from "axios";
-import { AppConfig } from "../Core/AppConfig";
-import { Constants_ServiceResultType } from "../Core/Constants";
-import { TextHelper } from "../Toolbox/TextHelper";
-import { AuthBusiness } from "./AuthBusiness";
+import { AppError } from "../platform/errors/appError";
+import { apiClient } from "../platform/http/httpClient";
+
+const ensureServiceResult = (result) => {
+    if (result && typeof result.isSuccess === "boolean") return result;
+    throw new AppError("Geçersiz API yanıtı.", {
+        code: "INVALID_RESPONSE",
+        retryable: false
+    });
+};
+
+const publicBootstrapOptions = (options = {}) => ({
+    ...options,
+    cache: true,
+    dedupe: !options.signal,
+    cacheTtlMs: options.cacheTtlMs ?? 120000
+});
 
 export const ConfigurationBusiness = {
-
-
-    GetMapConfiguration: async () => {
-
-        let _headers = await AuthBusiness.GetRequestHeaders();
-        
-        let url = AppConfig.Api.BaseUrl + '/AppSettings/List?key=GisMapConfig';
-
-        return new Promise((resolve, reject) => {
-
-            axios({
-                method: "get",
-                url: url,
-                headers: _headers,
-            }).then((result) => {
-
-                resolve(result.data);
-            });
-
+    GetMapConfiguration: async (options = {}) => {
+        const result = await apiClient.get("/AppSettings/List", {
+            ...publicBootstrapOptions(options),
+            params: { key: "GisMapConfig" }
         });
-
-
+        return ensureServiceResult(result);
     },
 
-    GetConfigServices: async () => {
-
-        let _headers = await AuthBusiness.GetRequestHeaders();
-
-        let url = AppConfig.Api.BaseUrl + '/Gis/ConfigService/List';
-
-        return new Promise((resolve, reject) => {
-
-            axios({
-                method: "get",
-                url: url,
-                headers: _headers,
-            }).then((result) => {
-
-                resolve(result.data);
-
-            });
-
-        });
+    GetConfigServices: async (options = {}) => {
+        const result = await apiClient.get(
+            "/Gis/ConfigService/List",
+            publicBootstrapOptions(options)
+        );
+        return ensureServiceResult(result);
     }
-
 };
