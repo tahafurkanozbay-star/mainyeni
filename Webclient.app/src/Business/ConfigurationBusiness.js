d919358fb53951473f53b4c4def6350467f1df9e
tahafurkanozbay-star <tahafurkan.ozbay@ogr.iu.edu.tr>
2026-09-15 15:10:21 +0300

    refactor(qa): simplify shared business request helpers

--- Webclient.app/src/Business/ConfigurationBusiness.js
+++ Webclient.app/src/Business/ConfigurationBusiness.js
@@ -1,53 +1,18 @@
 import axios from "axios";
 import { AppConfig } from "../Core/AppConfig";
-import { Constants_ServiceResultType } from "../Core/Constants";
-import { TextHelper } from "../Toolbox/TextHelper";
 import { AuthBusiness } from "./AuthBusiness";
 
+const get = async path => {
+    const headers = await AuthBusiness.GetRequestHeaders();
+    const result = await axios({
+        method: "get",
+        url: AppConfig.Api.BaseUrl + path,
+        headers
+    });
+    return result.data;
+};
+
 export const ConfigurationBusiness = {
-
-
-    GetMapConfiguration: async () => {
-
-        let _headers = await AuthBusiness.GetRequestHeaders();
-        
-        let url = AppConfig.Api.BaseUrl + '/AppSettings/List?key=GisMapConfig';
-
-        return new Promise((resolve, reject) => {
-
-            axios({
-                method: "get",
-                url: url,
-                headers: _headers,
-            }).then((result) => {
-
-                resolve(result.data);
-            });
-
-        });
-
-
-    },
-
-    GetConfigServices: async () => {
-
-        let _headers = await AuthBusiness.GetRequestHeaders();
-
-        let url = AppConfig.Api.BaseUrl + '/Gis/ConfigService/List';
-
-        return new Promise((resolve, reject) => {
-
-            axios({
-                method: "get",
-                url: url,
-                headers: _headers,
-            }).then((result) => {
-
-                resolve(result.data);
-
-            });
-
-        });
-    }
-
+    GetMapConfiguration: () => get('/AppSettings/List?key=GisMapConfig'),
+    GetConfigServices: () => get('/Gis/ConfigService/List')
 };
