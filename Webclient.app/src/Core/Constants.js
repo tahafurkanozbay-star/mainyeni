b37e58a8933e1d945b911ba87aac1f8df6cf0812
tahafurkanozbay-star <tahafurkan.ozbay@ogr.iu.edu.tr>
2026-09-15 12:44:13 +0300

    fix(ui): define persisted theme configuration key

--- Webclient.app/src/Core/Constants.js
+++ Webclient.app/src/Core/Constants.js
@@ -12,7 +12,6 @@ export const Constants_MessageType = {
     Error:"danger"
 }
 
-
 export const Constants_ServiceResultType = {
     Success: 10,
     Error: 20
@@ -25,52 +24,35 @@ export const Constants_LayerType = {
     GeoJSONLayer: 5
 }
 
-
-
 export const Constants_Symbols = {
-
     activeFillSymbol: {
-        type: "simple-fill", // autocasts as new SimpleFillSymbol()
+        type: "simple-fill",
         color: [55, 55, 55, 0.3],
-        outline: {
-            color: "#b0ff5b",
-            width: 3
-        }
+        outline: { color: "#b0ff5b", width: 3 }
     },
-
     polylineSymbol: {
         type: "simple-line",
         color: "#b0ff5b",
         width: 4
     },
-
     pointSymbol: {
         type: "simple-marker",
         style: "circle",
         size: 8,
         color: [64, 64, 255],
-        outline: {
-            color: "#b0ff5b",
-            width: 4
-        }
+        outline: { color: "#b0ff5b", width: 4 }
     },
-
     polygonSymbol: {
-        type: "simple-fill", // autocasts as new SimpleFillSymbol()
+        type: "simple-fill",
         color: [255, 255, 255, 0.7],
-        outline: {
-            // autocasts as new SimpleLineSymbol()
-            color: "#b0ff5b",
-            width: 3
-        }
+        outline: { color: "#b0ff5b", width: 3 }
     }
 }
 
-
 export const Constants_ConfigKeys={
     BOOKMARKS: "a8s1xiapslcqxefef34jk",
+    THEME_CHOICE: "kent-rehberi-theme-choice"
 };
-
 
 export const Constants_UserMesssages={
     LOCATION_ALLOWED:"Konumunuz kullanılıyor",
