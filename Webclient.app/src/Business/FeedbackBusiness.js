353a8bba88c6fb0c02759e76a2cb037a107046b7
tahafurkanozbay-star <tahafurkan.ozbay@ogr.iu.edu.tr>
2026-09-16 15:34:26 +0300

    feat(platform): migrate feedback requests to native http client

--- Webclient.app/src/Business/FeedbackBusiness.js
+++ Webclient.app/src/Business/FeedbackBusiness.js
@@ -1,40 +1,21 @@
-import axios from "axios";
 import { AppConfig } from "../Core/AppConfig";
 import { AuthBusiness } from "./AuthBusiness";
+import { HttpBusiness } from "./HttpBusiness";
 
-export const FeedbackBusiness={
+export const FeedbackBusiness = {
+    SendFeedBack: async formData => {
+        const headers = await AuthBusiness.GetRequestHeaders();
 
-    SendFeedBack:async (formData) => {
-        
-        let _headers = await AuthBusiness.GetRequestHeaders();
-
-        let config = {
-            method: 'post',
-            url: AppConfig.Api.BaseUrl + '/Feedback/Save',
-            headers: _headers,
-            data: JSON.stringify(formData),
-        };
-    
-        return new Promise((resolve) => {
-    
-            return axios(config)
-                .then(function (response) {
-    
-                    let result = response.data;
-                    resolve(result);
-                }).catch(function (error) {
-                    resolve(null);
-                });
-    
-        });
+        try {
+            return await HttpBusiness.Post(AppConfig.Api.BaseUrl + '/Feedback/Save', formData, { headers });
+        } catch {
+            return null;
+        }
     },
 
-    GetFeedbackTypes :() => {
-
-        return [
-            { id: 1, name: "Uygulama hakkında görüş ve tavsiye" },
-            { id: 2, name: "Adres sorunu bildirme" },
-            { id: 3, name: "Veri sorunu bildirme" },
-        ];
-    }
-}
+    GetFeedbackTypes: () => [
+        { id: 1, name: "Uygulama hakkında görüş ve tavsiye" },
+        { id: 2, name: "Adres sorunu bildirme" },
+        { id: 3, name: "Veri sorunu bildirme" },
+    ]
+};
