78c921096ec28e376868017f28ac088145310e15
tahafurkanozbay-star <tahafurkan.ozbay@ogr.iu.edu.tr>
2026-09-16 10:37:59 +0300

    refactor(web): move state store to Redux Toolkit

--- Webclient.app/src/Store/Store.js
+++ Webclient.app/src/Store/Store.js
@@ -1,16 +1,23 @@
-import { createStore, combineReducers } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import { ContextMenuReducer } from "./Reducers/ContextMenuReducer";
 import { MapReducer } from "./Reducers/MapReducer";
 import { CommonReducer } from "./Reducers/CommonReducer";
 import { DynamicLayersReducer } from "./Reducers/DynamicLayersReducer";
 
-const rootReducer = combineReducers({
-  DynamicLayers: DynamicLayersReducer,
-  ContextMenu: ContextMenuReducer,
-  Map: MapReducer,
-  Common: CommonReducer
+export const Store = configureStore({
+  reducer: {
+    DynamicLayers: DynamicLayersReducer,
+    ContextMenu: ContextMenuReducer,
+    Map: MapReducer,
+    Common: CommonReducer
+  },
+  // ArcGIS SDK objects are intentionally stored by legacy reducers today. Keep the
+  // migration behavior-compatible while gaining the modern Redux Toolkit store API.
+  // These checks can be enabled per-slice as the remaining GIS state becomes serializable.
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    immutableCheck: false,
+    serializableCheck: false
+  })
 });
-
-export const Store = createStore(rootReducer);
 
 export default Store;
