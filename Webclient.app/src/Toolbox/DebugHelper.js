13137c62640a24f5b885fd15778302a10c0017dd
tahafurkanozbay-star <tahafurkan.ozbay@ogr.iu.edu.tr>
2026-09-16 14:51:02 +0300

    fix(platform): use runtime config for debug logging

--- Webclient.app/src/Toolbox/DebugHelper.js
+++ Webclient.app/src/Toolbox/DebugHelper.js
@@ -1,14 +1,15 @@
-import {DatetimeHelper} from "./DatetimeHelper";
+import { runtimeConfig } from "../platform/config/runtimeConfig";
+import { DatetimeHelper } from "./DatetimeHelper";
 
 export const DebugHelper = {
 
     Log: (_message) => {
 
-        if (process.env.REACT_APP_ENV_DEBUG) {
+        if (runtimeConfig.environment !== "production") {
 
-            console.log(" --- [DEBUG-LOG] --- "+DatetimeHelper.GetFormatted(new Date()));
+            console.log(" --- [DEBUG-LOG] --- " + DatetimeHelper.GetFormatted(new Date()));
             console.log(_message);
-            
+
         }
     }
 }
