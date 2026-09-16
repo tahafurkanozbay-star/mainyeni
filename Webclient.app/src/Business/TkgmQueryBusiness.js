eca205d7713e5c9e2b28ef3f001bd14299699a40
tahafurkanozbay-star <tahafurkan.ozbay@ogr.iu.edu.tr>
2026-09-16 14:44:04 +0300

    fix(webclient): remove undeclared axios import from tkgm

--- Webclient.app/src/Business/TkgmQueryBusiness.js
+++ Webclient.app/src/Business/TkgmQueryBusiness.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from '../platform/http/legacyHttpClient';
 import { loadModules } from "esri-loader";
 import {AppConfig} from "../Core/AppConfig";
 import { IsNull } from "../Toolbox/ObjectHelper";
