9fc928a40ce688b60a4610c5527659920ee0ac10
tahafurkanozbay-star <tahafurkan.ozbay@ogr.iu.edu.tr>
2026-09-17 15:39:25 +0300

    refactor(experience): route error imports through TypeScript

--- Webclient.app/src/Components/Common/Error.js
+++ Webclient.app/src/Components/Common/Error.js
@@ -1,12 +1,1 @@
-import "./Error.css";
-
-export const FullScreenError = ({ message = "Beklenmeyen bir hata oluştu." }) => (
-    <div
-        className="w-100 h-100 full-screen-div"
-        role="alert"
-        aria-live="assertive"
-        aria-atomic="true"
-    >
-        <div className="ErrorFullScreenText">{message}</div>
-    </div>
-);
+export * from './Error.tsx';
