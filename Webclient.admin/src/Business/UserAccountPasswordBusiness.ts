1faa1672cb7178ee062f0055093cbf33ea28e8df
tahafurkanozbay-star <tahafurkan.ozbay@ogr.iu.edu.tr>
2026-09-21 16:09:07 +0300

    refactor(admin-api): centralize password-update transport

--- Webclient.admin/src/Business/UserAccountPasswordBusiness.ts
+++ Webclient.admin/src/Business/UserAccountPasswordBusiness.ts
@@ -1,13 +1,10 @@
-import axios from "axios";
 import { Constants } from "../Core/Constants";
-import { Global } from "../Core/Global";
 import { IsNull } from "../Core/Toolbox/ObjectHelper";
-import { AuthBusiness } from "./AuthBusiness";
+import { adminApiPost } from "../runtime/adminApiClient";
 
 export const UserAccountPasswordBusiness={
     
     Validate:(_item)=>{
-
         if (IsNull(_item.password)) {
             return { type: Constants.MessageTypes.Error, text: "Lütfen şifre giriniz" };
         }
@@ -25,34 +22,12 @@ export const UserAccountPasswordBusiness={
         return {type:"success",text:""};
     },
 
-
     Save: async (_itemDetails) => {
-
-        let _headers = await AuthBusiness.GetRequestHeaders();
-
-        return new Promise((resolve, reject) => {
-
-            let url = Global.API_URL + "/UserAccount/UpdatePassword";
-
-            axios({
-                method: "post",
-                url: url,
-                data: JSON.stringify({
-                    id: _itemDetails.id,
-                    password: _itemDetails.password,
-                    passwordRepeat: _itemDetails.passwordRepeat,
-                    sc: "[Security Key]"
-                }),
-                headers: _headers
-            })
-                .then(function (response) {
-                    var result = response.data;
-                    resolve(result);
-                })
-                .catch(function (error) {
-                    return AuthBusiness.HandleRejection(error);
-                });
+        return adminApiPost("/UserAccount/UpdatePassword", {
+            id: _itemDetails.id,
+            password: _itemDetails.password,
+            passwordRepeat: _itemDetails.passwordRepeat,
+            sc: "[Security Key]"
         });
     },
-
 };
