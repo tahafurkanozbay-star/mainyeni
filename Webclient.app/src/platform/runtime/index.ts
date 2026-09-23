f67d656080462da11e9babfd1c74641df26a44b8
tahafurkanozbay-star <tahafurkan.ozbay@ogr.iu.edu.tr>
2026-09-23 15:02:51 +0300

    feat(platform): export runtime degradation controller

--- Webclient.app/src/platform/runtime/index.ts
+++ Webclient.app/src/platform/runtime/index.ts
@@ -83,3 +83,4 @@ export * from './runtimeBackpressureCoordinator';
 export * from './runtimeQuarantineRegistry';
 export * from './runtimeConcurrencyGovernor';
 export * from './runtimeLoadWindow';
+export * from './runtimeDegradationController';
