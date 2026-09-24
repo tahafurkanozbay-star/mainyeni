b41dc4d5d61fbfa449ff6ffc637889f7c7d72e52
tahafurkanozbay-star <tahafurkan.ozbay@ogr.iu.edu.tr>
2026-09-24 10:39:19 +0300

    fix(search): satisfy strict changed-source lint

--- Webclient.app/src/data-search/searchWorkloadGovernor.ts
+++ Webclient.app/src/data-search/searchWorkloadGovernor.ts
@@ -325,7 +325,7 @@ export class SearchWorkloadGovernor {
     this.#disposed = true;
     const error = createDisposedError();
     error.message = reason;
-    for (const item of [...this.#queue]) {
+    for (const item of this.#queue.slice()) {
       this.#settleQueuedItem(item, 'disposed', error);
     }
     for (const item of this.#inflight.values()) {
@@ -492,7 +492,7 @@ export class SearchWorkloadGovernor {
       subscriberCount: item.subscribers.size,
     }));
 
-    for (const subscriber of [...item.subscribers]) {
+    for (const subscriber of Array.from(item.subscribers)) {
       if (subscriber.signal && subscriber.abortListener) {
         subscriber.signal.removeEventListener('abort', subscriber.abortListener);
       }
