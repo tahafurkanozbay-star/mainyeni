dbe3d677d14e60761fab5e7f65eb4235d87eb9f2
tahafurkanozbay-star <tahafurkan.ozbay@ogr.iu.edu.tr>
2026-09-25 23:02:34 +0300

    feat(platform): export dependency incident coordinator

--- Webclient.app/src/platform/runtime/index.ts
+++ Webclient.app/src/platform/runtime/index.ts
@@ -62,5 +62,6 @@ export * from './runtimeDependencyFailoverPlanner';
 export * from './runtimeDependencySloController';
 export * from './runtimeDependencyChangeGuard';
 export * from './runtimeDependencyDeploymentGate';
+export * from './runtimeDependencyIncidentCoordinator';
 export { RuntimePressureOrchestrator } from './runtimePressureOrchestrator';
 export type { RuntimePressureMode as RuntimeOrchestrationPressureMode, RuntimePressureDecision as RuntimeOrchestrationPressureDecision, RuntimePressureReason as RuntimeOrchestrationPressureReason, RuntimePressurePolicy as RuntimeOrchestrationPressurePolicy, RuntimePressureSample as RuntimeOrchestrationPressureSample, RuntimePressureRequest as RuntimeOrchestrationPressureRequest, RuntimePressureResult as RuntimeOrchestrationPressureResult, RuntimePressureTransition as RuntimeOrchestrationPressureTransition, RuntimePressureLaneSnapshot as RuntimeOrchestrationPressureLaneSnapshot, RuntimePressureSnapshot as RuntimeOrchestrationPressureSnapshot } from './runtimePressureOrchestrator';
