0c6c787709c7b01d4b3b2775639a01745e0c44b6
tahafurkanozbay-star <tahafurkan.ozbay@ogr.iu.edu.tr>
2026-09-25 20:04:02 +0300

    feat(platform): export dependency SLO controller

--- Webclient.app/src/platform/runtime/index.ts
+++ Webclient.app/src/platform/runtime/index.ts
@@ -64,5 +64,6 @@ export * from './runtimeDependencyHealthRegistry';
 export * from './runtimeDependencyTopology';
 export * from './runtimeDependencyReadinessCoordinator';
 export * from './runtimeDependencyFailoverPlanner';
+export * from './runtimeDependencySloController';
 export { RuntimePressureOrchestrator } from './runtimePressureOrchestrator';
 export type { RuntimePressureMode as RuntimeOrchestrationPressureMode, RuntimePressureDecision as RuntimeOrchestrationPressureDecision, RuntimePressureReason as RuntimeOrchestrationPressureReason, RuntimePressurePolicy as RuntimeOrchestrationPressurePolicy, RuntimePressureSample as RuntimeOrchestrationPressureSample, RuntimePressureRequest as RuntimeOrchestrationPressureRequest, RuntimePressureResult as RuntimeOrchestrationPressureResult, RuntimePressureTransition as RuntimeOrchestrationPressureTransition, RuntimePressureLaneSnapshot as RuntimeOrchestrationPressureLaneSnapshot, RuntimePressureSnapshot as RuntimeOrchestrationPressureSnapshot } from './runtimePressureOrchestrator';
